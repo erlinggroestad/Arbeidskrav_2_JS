c763b3ce0ad1665ed20119e0d74418c3b933e479
Rizo <ulrikrisemusic@gmail.com>
2022-03-29 18:16:44 +0100

    fetched api and tested array

--- js/classroom.js
+++ js/classroom.js
@@ -1,1 +1,16 @@
 let snapeDiv = document.getElementById("snape-div");
+
+async function getHpApi(url) {
+  const infoGather = await fetch(url);
+  const jsonData = await infoGather.json();
+  return jsonData;
+}
+
+const severusInfo = document.createElement("button");
+severusInfo.innerText = "Severus Snape info";
+document.body.append(severusInfo);
+severusInfo.addEventListener("click", () =>
+  getHpApi("http://hp-api.herokuapp.com/api/characters").then((jsonData) => {
+    console.log(jsonData[7]);
+  })
+);
