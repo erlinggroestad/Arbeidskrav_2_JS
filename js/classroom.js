a1e37f119f8b09929e8a9137089bba4cf1267048
Rizo <ulrikrisemusic@gmail.com>
2022-03-30 10:40:42 +0100

    fixed fetch api, added snapeInfo to js, html and css

--- js/classroom.js
+++ js/classroom.js
@@ -1,6 +1,7 @@
 let snapeDiv = document.getElementById("snape-div");
 let professorSnapeImg = document.getElementById("professor-snape-img");
 let speechBubble = document.getElementById("speech-bubble");
+let snapeInfo = document.getElementById("snape-info");
 
 async function getHpApi(url) {
   const infoGather = await fetch(url);
@@ -8,13 +9,17 @@ async function getHpApi(url) {
   return jsonData;
 }
 
-const classroomOpener = document.createElement("button");
-classroomOpener.innerText = "Open classroom";
-document.body.append(classroomOpener);
-classroomOpener.addEventListener("click", () =>
+fetch("http://hp-api.herokuapp.com/api/characters").then(
   getHpApi("http://hp-api.herokuapp.com/api/characters").then((jsonData) => {
     console.log(jsonData[7]);
     document.getElementById("professor-snape-img").src = jsonData[7].image;
+    snapeInfo.innerHTML =
+      "My name is " +
+      jsonData[7].name +
+      " I was born on the " +
+      jsonData[7].dateOfBirth +
+      ". My wand is made of an unknown material. ";
+
     professorSnapeImg.addEventListener("mouseenter", speechBubbleHover);
   })
 );
