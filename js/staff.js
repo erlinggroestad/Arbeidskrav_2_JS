02ed03a16a348cbbba526f9139a71edc13b6b752
erlinggroestad <erling.groestad@gmail.com>
2022-04-01 15:49:37 +0200

    added images folder, and made a default image for wizards with no image.

--- js/staff.js
+++ js/staff.js
@@ -8,19 +8,19 @@ fetch("http://hp-api.herokuapp.com/api/characters/staff").then(
   getStaffApi("http://hp-api.herokuapp.com/api/characters/staff").then(
     (jsonData) => {
       console.log(jsonData);
-      
 
-      let text = "";
+      let staffCardsInfo = "";
+      let noHouse = "hei";
 
       for (let i = 0; i < jsonData.length; i++) {
-        text += `<li>Name: ${jsonData[i].name} <br>
-         House:${jsonData[i].house} <br>
-         <img id="staff-img" src="${jsonData[i].image}" alt="" /> <br><br> </li>`;
+        staffCardsInfo += `<li>
+         <img id="staff-img" src="${jsonData[i].image}" alt=""   onerror="this.src='/images/wizard.png'" /> <br>
+         Name: ${jsonData[i].name} <br>
+         House:${jsonData[i].house} </li>`;
       }
-      document.getElementById("test").innerHTML = text;
+      document.getElementById("cards").innerHTML = staffCardsInfo;
 
-      if (jsonData.house === "") {
-      } else {
+      if (jsonData[i].house === "") {
       }
     }
   )
