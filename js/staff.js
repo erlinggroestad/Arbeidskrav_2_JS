e5207b437bbb1ea0ad59d90d39b40d8c3c2c52d9
erlinggroestad <erling.groestad@gmail.com>
2022-04-04 15:22:55 +0200

    Added styling on diffrent houses, added images, and not gotten the speachbubble to work-.-

--- js/staff.js
+++ js/staff.js
@@ -1,3 +1,6 @@
+let speechBubble = document.getElementById("patronus");
+let cards = document.getElementById("cards");
+
 async function getStaffApi(url) {
   let infoGather = await fetch(url);
   let jsonData = await infoGather.json();
@@ -12,16 +15,26 @@ fetch("http://hp-api.herokuapp.com/api/characters/staff").then(
       let staffCardsInfo = "";
 
       for (let i = 0; i < jsonData.length; i++) {
-        staffCardsInfo += `<li>
-         <img id="staff-img" src="${jsonData[i].image}" alt=""   
-         onerror="this.src='/images/wizard.png'" /> <br>
+        staffCardsInfo += `<li class="cards" id="${
+          `House-` + jsonData[i].house
+        }">
+         <img id="staff-img" src="${
+           jsonData[i].image
+         }" alt="" onerror="this.src='/images/wizard.png'" /> <br>
          Name: ${jsonData[i].name} <br>
-         House: ${jsonData[i].house} </li>`;
+         House: ${jsonData[i].house} <br>
+         <div id="patronus">Patronus: ${jsonData[i].patronus} </div></li>`;
+        // cards.addEventListener("mouseenter", speechBubbleHover);
       }
-      document.getElementById("cards").innerHTML = staffCardsInfo;
-
-      if (jsonData[i].house === "") {
-      }
+      document.getElementById("staff-cards").innerHTML = staffCardsInfo;
     }
   )
 );
+function speechBubbleHover() {
+  speechBubble.style.opacity = 100;
+  cards.addEventListener("mouseleave", speechBubbleLeave);
+  alert("Hello");
+}
+function speechBubbleLeave() {
+  speechBubble.style.opacity = 0;
+}
