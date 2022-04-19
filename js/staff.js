b1243e6b59a3bc8125ed21973d4cd62c1eb2e945
erlinggroestad <erling.groestad@gmail.com>
2022-04-19 18:27:48 +0200

    added edit function, only works on the first card as of now

--- js/staff.js
+++ js/staff.js
@@ -6,6 +6,8 @@ async function getStaffApi(url) {
   return jsonData;
 }
 
+function staffFromApi() {}
+
 fetch("http://hp-api.herokuapp.com/api/characters/staff").then(
   getStaffApi("http://hp-api.herokuapp.com/api/characters/staff").then(
     (jsonData) => {
@@ -14,27 +16,33 @@ fetch("http://hp-api.herokuapp.com/api/characters/staff").then(
       let staffCardsInfo = "";
 
       for (let i = 0; i < jsonData.length; i++) {
-        staffCardsInfo += `<li class="cards" id="${
+        staffCardsInfo += `<li class="cards" ${jsonData[i].name}  id="${
           `House-` + jsonData[i].house
         }">
+        <div id="cards__container">
+        <button type="submit" id="edit-button">Edit</button>
+        <button type="submit" id="end-editing">Done</button> <br>
+ 
         <button onclick="deleteStaff()"> Delete me </button> <br>
         <img id="staff-img" src="${
           jsonData[i].image
         }" alt="" onerror="this.src='/images/wizard.png'" /> <br>
 
         <div id="staff-text-info" <br>
-        <div id="staff-name">   Name: ${jsonData[i].name}</div>
-        <div id="staff-house">  House: ${jsonData[i].house}</div>
-        <div id="patronus">     Patronus: ${jsonData[i].patronus}</div>
-        </li>`;
+        Name: <div id="staff-name">    ${jsonData[i].name}</div>
+        House: <div id="staff-house">   ${jsonData[i].house}</div>
+        Patronus: <div id="patronus"> ${jsonData[i].patronus}</div>
+        </div></li>`;
       }
       document.getElementById("staff-cards").innerHTML = staffCardsInfo;
+      editFunc();
     }
   )
 );
+
 addedStaffArray = [];
 
-
+// Trenger å fikse edit på alle ansatte og slette funksjon
 
 function addStaff() {
   let inputName = document.getElementById("select-name").value;
@@ -53,30 +61,60 @@ function addStaff() {
   }
 
   function addedStaffCards() {
-    let newStaffCardsInfo = "";
+    let staffCardsInfo = "";
     for (let i = 0; i < addedStaffArray.length; i++) {
-      newStaffCardsInfo += `<li class="cards" id="${
+      staffCardsInfo += `<li class="cards" id="${
         `House-` + addedStaffArray[i].house
       }">
+        <div id="cards__container">
+        <button type="submit" id="edit-button">Edit</button>
+        <button type="submit" id="end-editing">Done</button><br>
 
         <button onclick="deleteStaff()"> Delete me </button> <br>
         <img id="staff-img" src="" alt="" onerror="this.src='/images/wizard.png'" /> <br>
 
         <div id="staff-text-info" <br>
-        <div id="staff-name">   Name: ${addedStaffArray[i].name}</div>
-        <div id="staff-house">  House: ${addedStaffArray[i].house} </div>
-        <div id="patronus">     Patronus: ${addedStaffArray[i].patronus}</div>
-        </li>`;
+        Name: <div id="staff-name">    ${addedStaffArray[i].name}</div>
+        House: <div id="staff-house">   ${addedStaffArray[i].house}</div>
+        Patronus: <div id="patronus"> ${addedStaffArray[i].patronus}</div>
+        </div></li>`;
     }
-    document.getElementById("staff-cards").innerHTML = newStaffCardsInfo;
+    document.getElementById("staff-cards").innerHTML = staffCardsInfo;
+    editFunc();
   }
 }
 
-// function deleteStaff(i) {
-//   let confirmDelete = prompt("Are you sure you want too delete? yes/no");
-//   if (confirmDelete == "yes") {
-//     addedStaffArray.splice(i, 1);
-//     addedStaffCards();
-//   }
-// }
-// console.log(addedStaffArray);
+function deleteStaff(i) {
+  let confirmDelete = prompt("Are you sure you want too delete? yes/no");
+  if (confirmDelete == "yes") {
+    addedStaffArray.splice(i, 1);
+  }
+}
+function editFunc() {
+  const editStaffName = document.getElementById("staff-name");
+  const editStaffHouse = document.getElementById("staff-house");
+  const editStaffPatronus = document.getElementById("patronus");
+
+  const edit_button = document.getElementById("edit-button");
+  const end_button = document.getElementById("end-editing");
+
+  edit_button.addEventListener("click", function () {
+    editStaffName.contentEditable = true;
+    editStaffHouse.contentEditable = true;
+    editStaffPatronus.contentEditable = true;
+
+    editStaffName.style.backgroundColor = "#C4A484";
+    editStaffHouse.style.backgroundColor = "#C4A484";
+    editStaffPatronus.style.backgroundColor = "#C4A484";
+  });
+
+  end_button.addEventListener("click", function () {
+    editStaffName.contentEditable = false;
+    editStaffHouse.contentEditable = false;
+    editStaffPatronus.contentEditable = false;
+
+    editStaffName.style.backgroundColor = null;
+    editStaffHouse.style.backgroundColor = null;
+    editStaffPatronus.style.backgroundColor = null;
+  });
+}
