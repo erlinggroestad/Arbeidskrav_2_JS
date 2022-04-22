eac1abc7e1d2b7fdc66e112ff515c0664a51e780
Rizo <ulrikrisemusic@gmail.com>
2022-04-22 16:08:35 +0100

    editing now works completely but styling is gone

--- js/staff.js
+++ js/staff.js
@@ -11,63 +11,85 @@ async function getStaffApi(url) {
   });
 }
 function createStaffCards() {
+  let staffCardUl = document.getElementById("staff-cards")
   let staffCardsInfo = "";
   for (let i = 0; i < staffArray.length; i++) {
-    staffCardsInfo += `<li class="cards" id="${`House-` + staffArray[i].house}">
-        <div id="cards__container">
-        
-        <button  id="end-editing">Done</button> <br>
+    // staffCardsInfo += `<li class="cards" id="${`House-` + staffArray[i].house}">
+    //     <div id="cards__container">
  
-        <button onclick="deleteStaff(${i})"> Delete me </button> <br>
-        <img id="staff-img" src="${
-          staffArray[i].image
-        }" alt="" onerror="this.src='/images/wizard.png'" /> <br>
+    //     <button onclick="deleteStaff(${i})"> Delete me </button> <br>
+    //     <img id="staff-img" src="${
+    //       staffArray[i].image
+    //     }" alt="" onerror="this.src='/images/wizard.png'" /> <br>
 
-        <div id="staff-text-info" <br>
-        Name: <div id="staff-name">    ${staffArray[i].name}</div>
-        <button  id="name-button" onclick="editName(${i})">Edit Name</button> <br
-        >
-        House: <div id="staff-house">  ${staffArray[i].house}</div>
-        <button  id="house-button" onclick="editHouse(${i})">Edit House</button>
-        <div id="patronus">Patronus:<div id="change-patronus">${
-          staffArray[i].patronus
-        }</div>
-        <button  id="patronus-button" onclick="editPatronus(${i})">Edit Patronus</button>
-        </div></li>`;
+    //     <div id="staff-text-info"> <br>
+    //     Name: <div id="staff-name">    ${staffArray[i].name}</div>
+    //     <button  id="name-button" onclick="editName(${i})">Edit Name</button> <br
+    //     >
+    //     House: <div id="staff-house">  ${staffArray[i].house}</div>
+    //     <button  id="house-button" onclick="editHouse(${i})">Edit House</button>
+    //     <div id="patronus">Patronus:<div id="change-patronus">${
+    //       staffArray[i].patronus
+    //     }</div>
+    //     <button  id="patronus-button" onclick="editPatronus(${i})">Edit Patronus</button>
+    //     </div></li>`;
+
+let cards = document.createElement("li");
+cards.setAttribute = ("id", "staffArray[i].house");
+let deleteBtn = document.createElement("button");
+deleteBtn.addEventListener("click",() =>  {deleteStaff(i)});
+let staffImg = document.createElement("img");
+staffImg.src = staffArray[i].image;
+let staffTextInfo = document.createElement("div");
+let staffName = document.createElement("div");
+staffName.innerHTML = staffArray[i].name;
+let nameBtn = document.createElement("button");
+nameBtn.addEventListener("click", () => {editName(i, staffName, staffHouse, staffPatronus);});
+nameBtn.innerHTML = "Change name"
+let staffHouse = document.createElement("div");
+staffHouse.innerHTML = staffArray[i].house;
+let houseBtn = document.createElement("button");
+houseBtn.addEventListener("click",() =>  {editHouse(i, staffName, staffHouse, staffPatronus);});
+houseBtn.innerHTML = "Change house"
+let staffPatronus = document.createElement("div");
+staffPatronus.innerHTML = staffArray[i].patronus;
+let patronusBtn = document.createElement("button");
+patronusBtn.addEventListener("click", () =>  {editPatronus(i, staffName, staffHouse, staffPatronus);});
+patronusBtn.innerHTML = "Change patronus"
+
+staffTextInfo.append(staffName, nameBtn, staffHouse, houseBtn, staffPatronus, patronusBtn)
+cards.append(deleteBtn, staffImg, staffTextInfo)
+staffCardUl.append(cards)
   }
-
-  document.getElementById("staff-cards").innerHTML = staffCardsInfo;
 }
 
-function editName(i) {
+function editName(i, staffName, staffHouse, staffPatronus) {
   let namePrompt = prompt("Do you want to change this name? yes/no");
   if (namePrompt == "yes") {
     let answerPrompt = prompt("What is the new name?");
     staffArray[i].name = answerPrompt;
-    let staffNameDiv = document.getElementById("staff-name");
-    staffNameDiv.innerHTML = answerPrompt;
+   staffName.innerHTML = answerPrompt;
   } else {
     console.log(i);
+    console.log(staffArray[i])
   }
 }
-function editHouse(i) {
+function editHouse(i, staffName, staffHouse, staffPatronus) {
   let housePrompt = prompt("Do you want to change this name? yes/no");
   if (housePrompt == "yes") {
     let answerPrompt = prompt("What is the new name?");
     staffArray[i].house = answerPrompt;
-    let staffHouseDiv = document.getElementById("staff-house");
-    staffHouseDiv.innerHTML = answerPrompt;
+    staffHouse.innerHTML = answerPrompt;
   } else {
     console.log(i);
   }
 }
-function editPatronus(i) {
+function editPatronus(i, staffName, staffHouse, staffPatronus) {
   let patronusPrompt = prompt("Do you want to change this name? yes/no");
   if (patronusPrompt == "yes") {
     let answerPrompt = prompt("What is the new name?");
     staffArray[i].patronus = answerPrompt;
-    let staffPatronusDiv = document.getElementById("change-patronus");
-    staffPatronusDiv.innerHTML = answerPrompt;
+    staffPatronus.innerHTML = answerPrompt;
   } else {
     console.log(i);
   }
