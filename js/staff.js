3874cdb7ac374d7b0b32e9b1dfa652a77f95e2cc
erlinggroestad <erling.groestad@gmail.com>
2022-04-22 02:25:01 +0200

    fixed deletefunc, add func, new staff adds to same array. Only thing left is the edit func

--- js/staff.js
+++ js/staff.js
@@ -1,140 +1,141 @@
 let createStaffBtn = document.getElementById("create-staff-btn");
 
+let staffArray = [];
+console.log(staffArray, "Staff from API");
+
 async function getStaffApi(url) {
-  let infoGather = await fetch(url);
-  let jsonData = await infoGather.json();
-  return jsonData;
+  const infoGather = await fetch(url);
+  const jsonData = await infoGather.json();
+  jsonData.forEach((staff) => {
+    staffArray.push(staff);
+  });
+}
+function createStaffCards() {
+  let staffCardsInfo = "";
+  for (let i = 0; i < staffArray.length; i++) {
+    staffCardsInfo += `<li class="cards" id="${`House-` + staffArray[i].house}">
+        <div id="cards__container">
+        <button type="submit" id="edit-button" onclick="editFunc(${i})">Edit</button>
+        <button type="submit" id="end-editing">Done</button> <br>
+ 
+        <button onclick="deleteStaff(${i})"> Delete me </button> <br>
+        <img id="staff-img" src="${
+          staffArray[i].image
+        }" alt="" onerror="this.src='/images/wizard.png'" /> <br>
+
+        <div id="staff-text-info" <br>
+        Name: <div id="staff-name">    ${staffArray[i].name}</div>
+        House: <div id="staff-house">   ${staffArray[i].house}</div>
+        <div id="patronus">Patronus:<div id="change__patronus">${
+          staffArray[i].patronus
+        }</div>
+        </div></li>`;
+  }
+
+  document.getElementById("staff-cards").innerHTML = staffCardsInfo;
 }
 
 fetch("http://hp-api.herokuapp.com/api/characters/staff").then(
   getStaffApi("http://hp-api.herokuapp.com/api/characters/staff").then(
     (jsonData) => {
-      let staffCardsInfo = "";
-
-      for (let i = 0; i < jsonData.length; i++) {
-        staffCardsInfo += `<li class="cards" id="${
-          `House-` + jsonData[i].house
-        }">
-        <div id="cards__container">
-        <button type="submit" id="edit-button" onclick="editFunc(${i})">Edit</button>
-        <button type="submit" id="end-editing">Done</button> <br>
- 
-        <button onclick="deleteStaff1(${i})"> Delete me </button> <br>
-        <img id="staff-img" src="${
-          jsonData[i].image
-        }" alt="" onerror="this.src='/images/wizard.png'" /> <br>
-
-        <div id="staff-text-info" <br>
-        Name: <div id="staff-name">    ${jsonData[i].name}</div>
-        House: <div id="staff-house">   ${jsonData[i].house}</div>
-        <div id="patronus">Patronus:<div id="change__patronus">${
-          jsonData[i].patronus
-        }</div>
-        </div></li>`;
-      }
-      document.getElementById("staff-cards").innerHTML = staffCardsInfo;
-      console.log(jsonData);
+      createStaffCards();
     }
   )
 );
-function deleteStaff1(i) {
+
+function deleteStaff(i) {
   let confirmDelete = prompt("Are you sure you want too delete? yes/no");
   if (confirmDelete == "yes") {
-    jsonData.splice(i, 1);
-    addStaff();
+    staffArray.splice(i, 1);
+  } else if (confirmDelete == "Yes") {
+    staffArray.splice(i, 1);
+  } else if (confirmDelete == "YES") {
+    staffArray.splice(i, 1);
   }
+  createStaffCards();
 }
 
-// Trenger å fikse edit på alle ansatte og slette funksjon
-let addedStaffArray = [];
+// Trenger å fikse edit på alle ansatte
 
 function addStaff() {
   let inputName = document.getElementById("select-name").value;
   let inputPatronus = document.getElementById("select-patronus").value;
   let result = document.getElementById("select").value;
-
-  // Printer selvom inputfeltene er tomme.
-  if ((inputName, inputPatronus === "")) {
-    alert("Feltene over kan ikke være tomme");
-  } else if (checkUserAnswer) {
-    areYouSure = prompt("Do you wish to create?");
-  } else areYouSure === "yes";
-  addedStaffArray.push({
-    name: inputName,
-    patronus: inputPatronus,
-    house: result,
-  });
-
-  addedStaffCards();
-}
-
-function checkUserAnswer() {
-  let areYouSure = prompt("Do you wish to create?");
+  var areYouSure = prompt("Are you sure? yes/no ");
+  if (inputName === "") {
+    alert("OBS! Feltene kan ikke være tomme");
+  } else if (inputPatronus === "") {
+    alert("OBS! Feltene kan ikke være tomme");
+  } else if (areYouSure == "yes") {
+    staffArray.push({
+      newName: inputName,
+      newPatronus: inputPatronus,
+      newHouse: result,
+    });
+    addedStaffCards();
+  }
 }
 
 function addedStaffCards() {
   let newStaffCardsInfo = "";
-  for (let i = 0; i < addedStaffArray.length; i++) {
+  for (let i = 25; i < staffArray.length; i++) {
     newStaffCardsInfo += `<li class="cards" id="${
-      `House-` + addedStaffArray[i].house
+      `House-` + staffArray[i].newHouse
     }">
         <div id="cards__container">
         <button type="submit" id="edit-button" onclick="editFunc(${i})">Edit</button>
         <button type="submit" id="end-editing">Done</button><br>
 
-        <button onclick="deleteStaff(${i})"> Delete me </button> <br>
+        <button onclick="deleteStaff1(${i})"> Delete me </button> <br>
         <img id="staff-img" src="" alt="" onerror="this.src='/images/wizard.png'" /> <br>
 
         <div id="staff-text-info" <br>
-        Name: <div id="staff-name">    ${addedStaffArray[i].name}</div>
-        House: <div id="staff-house">   ${addedStaffArray[i].house}</div>
+        Name: <div id="staff-name">    ${staffArray[i].newName}</div>
+        House: <div id="staff-house">   ${staffArray[i].newHouse}</div>
         <div id="patronus">Patronus:<div id="change__patronus">  ${
-          addedStaffArray[i].patronus
+          staffArray[i].newPatronus
         }</div>
         </div></li>`;
   }
 
-  document.getElementById("staff-cards").innerHTML = newStaffCardsInfo;
+  document.getElementById("new__staff__cards").innerHTML = newStaffCardsInfo;
 }
-function deleteStaff(i) {
+
+function deleteStaff1(i) {
   let confirmDelete = prompt("Are you sure you want too delete? yes/no");
   if (confirmDelete == "yes") {
-    addedStaffArray.splice(i, 1);
-    addedStaffCards();
+    staffArray.splice(i, 1);
   }
+  addedStaffCards();
 }
 
-console.log(addedStaffArray);
+// function editFunc() {
+//   const editStaffName = document.getElementById("staff-name");
+//   const editStaffHouse = document.getElementById("staff-house");
+//   const editStaffPatronus = document.getElementById("change__patronus");
 
-function editFunc() {
-  const editStaffName = document.getElementById("staff-name");
-  const editStaffHouse = document.getElementById("staff-house");
-  const editStaffPatronus = document.getElementById("change__patronus");
+//   const edit_button = document.getElementById("edit-button");
+//   const end_button = document.getElementById("end-editing");
 
-  const edit_button = document.getElementById("edit-button");
-  const end_button = document.getElementById("end-editing");
+//   edit_button.addEventListener("click", function () {
+//     editStaffName.contentEditable = true;
+//     editStaffHouse.contentEditable = true;
+//     editStaffPatronus.contentEditable = true;
 
-  edit_button.addEventListener("click", function () {
-    editStaffName.contentEditable = true;
-    editStaffHouse.contentEditable = true;
-    editStaffPatronus.contentEditable = true;
+//     editStaffName.style.backgroundColor = "#C4A484";
+//     editStaffHouse.style.backgroundColor = "#C4A484";
+//     editStaffPatronus.style.backgroundColor = "#C4A484";
+//   });
 
-    editStaffName.style.backgroundColor = "#C4A484";
-    editStaffHouse.style.backgroundColor = "#C4A484";
-    editStaffPatronus.style.backgroundColor = "#C4A484";
-  });
+//   end_button.addEventListener("click", function () {
+//     editStaffName.contentEditable = false;
+//     editStaffHouse.contentEditable = false;
+//     editStaffPatronus.contentEditable = false;
 
-  end_button.addEventListener("click", function () {
-    editStaffName.contentEditable = false;
-    editStaffHouse.contentEditable = false;
-    editStaffPatronus.contentEditable = false;
-
-    editStaffName.style.backgroundColor = null;
-    editStaffHouse.style.backgroundColor = null;
-    editStaffPatronus.style.backgroundColor = null;
-  });
-}
+//     editStaffName.style.backgroundColor = null;
+//     editStaffHouse.style.backgroundColor = null;
+//     editStaffPatronus.style.backgroundColor = null;
+//   });
+// }
 
 // Edit funker bare på første cardet, i cardsa fra apiet og de man lager.
-// Splice sletter fra addedstaffarrayet
-// Må få til å legge nye staff etter de fra API
